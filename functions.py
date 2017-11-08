ab9ea1e310bcbfc7b45d722aca68f81fbb824430
Damien Picard <dam.pic@free.fr>
2017-11-08 15:55:09 +0100

    Cleanup

--- functions.py
+++ functions.py
@@ -2,16 +2,16 @@ import bpy
 import bgl
 
 
-def remap(value, leftMin, leftMax, rightMin, rightMax):
-    # Figure out how 'wide' each range is
-    leftSpan = leftMax - leftMin
-    rightSpan = rightMax - rightMin
-
-    # Convert the left range into a 0-1 range (float)
-    valueScaled = (value - leftMin) / leftSpan
-
-    # Convert the 0-1 range into a value in the right range.
-    return rightMin + valueScaled * rightSpan
+# def remap(value, leftMin, leftMax, rightMin, rightMax):
+#     # Figure out how 'wide' each range is
+#     leftSpan = leftMax - leftMin
+#     rightSpan = rightMax - rightMin
+#
+#     # Convert the left range into a 0-1 range (float)
+#     valueScaled = (value - leftMin) / leftSpan
+#
+#     # Convert the 0-1 range into a value in the right range.
+#     return rightMin + valueScaled * rightSpan
 
 
 def draw_callback_3d(self, context):
@@ -20,22 +20,17 @@ def draw_callback_3d(self, context):
     bgl.glPointSize(4)
     bgl.glLineWidth(3)
 
-    count = 0
-    for index, value in self.frames.items():
-        if count != 0 and count != len(self.frames)-1:
-            bgl.glColor4f(1, 1, 0.0, 0.5)
-        elif count != len(self.frames)-1:
+    for f_i, frame in enumerate(self.frames.values()):
+        if f_i == 0:  # and f_i != len(self.frames)-1:
             bgl.glColor4f(1, 1, 1, 0.5)
-        else:
+        elif f_i != len(self.frames) - 1:
             bgl.glColor4f(1, 1, 0.0, 0.5)
 
         bgl.glBegin(bgl.GL_POINTS)
-        for point in value['plain']:
+        for point in frame['plain']:
             bgl.glVertex3f(*point)
 
         bgl.glEnd()
-
-        count += 1
 
     # Restore opengl defaults
     bgl.glPointSize(1)
